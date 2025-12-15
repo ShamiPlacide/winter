c4891026c4f5a75aaf6fd2153cc2f60c247ac3c8
ShamiPlacide <p.igabe@alustudent.com>
2025-12-15 19:28:14 +0200

    REST API with extra query

--- API/main.py
+++ API/main.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Waguan, welcome to my API!'
+
+@app.route("/user")
+def get_user():
+  user = [{
+    "name": "John",
+    "address": "KG 23 St",
+    "phone": "0788 975 642"
+  }, {
+    "name": "Jane",
+    "address": "KG 20 St",
+    "phone": "0788 975 641"
+  }]
+  extra = request.args.get('extra')
+  if extra:
+    user['extra'] = extra
+
+  return jsonify(user), 200
 
 if __name__ == '__main__':
   app.run(debug=True)
